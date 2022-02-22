184584d9d95b715f5f065539822598b9ac546a5d
BensonFeng <bensonfeng@outlook.com>
2022-02-21 22:28:26 -0500

    card component structure

---
+++ components/card.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = (props) => {
+  return (
+    <Link href={props.href}>
+      <a>
+        <h2>{props.name}</h2>
+        <Image
+          src={props.imgUrl}
+          width={260}
+          height={160}
+          alt="DarkHorse Coffee"
+        ></Image>
+      </a>
+    </Link>
+  );
+};
+
+export default Card;
