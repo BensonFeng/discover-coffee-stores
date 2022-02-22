b90ae0985c853f11ecba3d58de4429805ff8b326
BensonFeng <bensonfeng@outlook.com>
2022-02-21 23:33:01 -0500

    next.config unsplash img url

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
+import coffeeStores from "../data/coffee-stores.json";
 export default function Home() {
   const handleOnBannerBtnClick = () => {
     console.log("hi banner button");
@@ -31,18 +32,16 @@ export default function Home() {
           />
         </div>
         <div className={styles.cardLayout}>
-          <Card
-            name="DarkHorse Coffee"
-            imgUrl="/static/hero-image.png"
-            href="/coffee-store/darkhorse-coffee"
-            className={styles.card}
-          />
-          <Card
-            name="DarkHorse Coffee"
-            imgUrl="/static/hero-image.png"
-            href="/coffee-store/darkhorse-coffee"
-            className={styles.card}
-          />
+          {coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+                key={coffeeStore.name}
+              />
+            );
+          })}
         </div>
       </main>
     </div>
