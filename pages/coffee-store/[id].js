e1f496d4ec35fdce4d448b9c2db1a92f6cf66473
BensonFeng <bensonfeng@outlook.com>
2022-02-22 18:14:18 -0500

    getstaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -2,10 +2,34 @@ import Link from "next/link";
 
 import { useRouter } from "next/router";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  console.log("params", params);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    }, // will be passed to the page component as props
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
   const { id } = router.query;
-  console.log(id);
+
+  console.log("router", router);
+  console.log("props", props);
+
   return (
     <div>
       Coffee Store Page {id}
@@ -15,6 +39,8 @@ const CoffeeStore = () => {
       <Link href="/coffee-store/">
         <a>Back to home</a>
       </Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
