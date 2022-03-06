1dc6f840a680a3d8fd870d3e393b428b09324a64
BensonFeng <bensonfeng@outlook.com>
2022-03-05 21:30:37 -0500

    api PUT request counter

---
+++ pages/api/favouriteCoffeeStoreById.js
@@ -0,0 +1,46 @@
+import {
+  table,
+  getMinifiedRecords,
+  findRecordByFilter,
+} from "../../lib/airtable";
+
+const favouriteCoffeeStoreById = async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const { id } = req.body;
+
+      if (id) {
+        const records = await findRecordByFilter(id);
+
+        if (records.length !== 0) {
+          const record = records[0];
+          const calculateVoting = parseInt(record.voting) + parseInt(1);
+
+          const updateRecord = await table.update([
+            {
+              id: record.recordId,
+              fields: {
+                voting: calculateVoting,
+              },
+            },
+          ]);
+
+          if (updateRecord) {
+            const minifiedRecords = getMinifiedRecords(updateRecord);
+            res.json(minifiedRecords);
+          }
+        } else {
+          res.json({ message: "coffeee store id doesn't exist", id });
+        }
+      } else {
+        res.status(400);
+        res.json({ message: "Id is missing" });
+      }
+    } catch (err) {
+      res.status(500);
+      res.json({ message: "Error upvoting coffee Store", err });
+    }
+  }
+};
+
+export default favouriteCoffeeStoreById;
